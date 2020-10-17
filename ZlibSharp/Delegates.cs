0759ee13b3114216380000a7ee212b0cc2c25ba4
Michal Piatkowski <piatkowski2002@gmail.com>
2020-10-18 01:55:19 +0200

    Added C-to-C# Generator (Sichem)

---
+++ ZlibSharp/Delegates.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ZlibSharp
+{
+	public unsafe delegate void* zalloc_delegate(void* opaque, uint count, uint elementSize);
+
+	public unsafe delegate void zfree_delegate(void* opaque, void* ptr);
+
+	public unsafe delegate block_state deflate_func(internal_state* state, int flush);
+
+	public unsafe delegate uint inflate_func(void* in_desc, byte** next);
+}
